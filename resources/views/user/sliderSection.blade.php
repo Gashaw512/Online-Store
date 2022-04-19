2e8691896a18f7a7138959511032fae6280be453
Gashaw Kidane <kidanugashaw@gmail.com>
2022-04-19 21:09:25 +0300

    new componet to side bar slider created

---
+++ resources/views/user/sliderSection.blade.php
@@ -0,0 +1,74 @@
+<div class="head_top"  >
+    <section class="slider_section">
+        <div class="banner_main">
+            <div class="container-fluid padding3">
+                <div class="row">
+                    <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12 mapimg">
+                        <div class="text-bg">
+                            <h1>Stand <br>
+                                out<br>
+                                in <br>
+                                the<br>
+                                crowd</h1>
+                            <span>A new type of storefront</span>
+                            <a href="{{ route('register') }}">Free trial</a>
+                        </div>
+                    </div>
+                    <div class="col-xl-8 col-lg-8 col-md-8 col-sm-12">
+                        <div id="myCarousel" class="carousel slide banner_Client" data-ride="carousel">
+                            <ol class="carousel-indicators">
+                                <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                                <li data-target="#myCarousel" data-slide-to="1"></li>
+                                <li data-target="#myCarousel" data-slide-to="2"></li>
+                            </ol>
+                            <div class="carousel-inner">
+                                <div class="carousel-item active">
+                                    <div class="container">
+                                        <div class="carousel-caption text">
+                                            <div class="row">
+                                                <div class="col-md-12">
+                                                    <div class="img_bg">
+                                                        <figure><img src="images/cap.png" /></figure>
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="carousel-item">
+                                    <div class="container">
+                                        <div class="carousel-caption text">
+                                            <div class="row">
+                                                <div class="col-md-12">
+                                                    <div class="img_bg">
+                                                        <figure><img src="images/cap.png" /></figure>
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="carousel-item">
+                                    <div class="container">
+                                        <div class="carousel-caption text">
+                                            <div class="row">
+                                                <div class="col-md-12">
+                                                    <div class="img_bg">
+                                                        <figure><img src="images/cap.png" /></figure>
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
