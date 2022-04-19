00e224271c2f87cd75e1700f5b7e34b9203f07fe
Gashaw Kidane <kidanugashaw@gmail.com>
2022-04-19 21:11:37 +0300

    formating

--- resources/views/user/categoriesLink.blade.php
+++ resources/views/user/categoriesLink.blade.php
@@ -1,19 +1,9 @@
 
 <div class="row">
-   
     <div class="col-md-12">
         <div class="title" id="categoriesNav">
             <h2> Categories</h2>
             <ul class="categiri">
-        
-                {{-- <li><a href="/{{Auth::id()}}/Electronics">Electronics</a></li>
-                <li><a href="/{{Auth::id()}}/shoes">Shoes</a></li>
-                <li><a href="/{{Auth::id()}}/Watches">Watches</a></li>
-                <li><a href="/{{Auth::id()}}/jewellery">Jewellery</a></li>
-                <li><a href="#">Health and Beauty</a></li>
-                <li><a href="/{{Auth::id()}}/kids">Kids and Babies</a></li>
-                <li><a href="/{{Auth::id()}}/Sport">Sports</a></li>
-                <li><a href="#">Home and Garden</a></li> --}}
 
                 <li class="active"><a href="/">All Products</a></li>
                 <li><a href="/clothes">Clothing</a></li>
@@ -23,8 +13,6 @@
                 <li><a href="/jewellery">Jewellery</a></li>
                 <li><a href="/kids">Kids and Babies</a></li>
                 <li><a href="/sport">Sports</a></li>
-                <li><a href="#">Home and Garden</a></li>
-                <li><a href="#">Health and Beauty</a></li>
             </ul>
         </div>
     </div>
