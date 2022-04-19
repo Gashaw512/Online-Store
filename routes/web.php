160d85ed7a62f160adf197ed8b7b1fcc25991d20
Gashaw Kidane <kidanugashaw@gmail.com>
2022-04-19 21:07:04 +0300

    guard to web route

--- routes/web.php
+++ routes/web.php
@@ -5,29 +5,12 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\CategoryController;
-// use App\Models\Product;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index']);
-Route::get('/add_product', function(){
-    return view('products.add');
-});
-
-Route::post('/save', [ProductController::class, "add_product"]);
 
 
-Route::get('/updateView', [ProductController::class, "updateView"]);
-// Route::post('/users', [ProductController::class, "add_product"]);
 
-// Admin action
-Route::get('/stores', [AdminController::class, "stores"]);
-Route::get('/admin', [AdminController::class, "adminAdd"]);
-Route::get('/customerList', [AdminController::class, "showCustomer"]);;
-Route::get('/productList', [AdminController::class, "showProduct"]);
-Route::get('/delateProduct/{id}', [AdminController::class, "delateProduct"]);
-
-
-Route::get('/home', [HomeController::class, 'redirect']); // it is executed after use is loged in
 
 Route::middleware([
     'auth:sanctum',
@@ -37,9 +20,10 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
-});
-
-// Category controller??
+    Route::get('/home', [HomeController::class, 'redirect'])->name('home');
+    Route::get('/add_product', function(){
+        return view('products.add');
+    });
 Route::get('clothes',[CategoryController::class,'viewCloth']);
 Route::get('electronics',[CategoryController::class,'viewElectronic']);
 Route::get('shoes',[CategoryController::class,'viewShoes']);
@@ -47,5 +31,12 @@ Route::get('watches',[CategoryController::class,'viewWatches']);
 Route::get('jewellery',[CategoryController::class,'viewJewellery']);
 Route::get('sport',[CategoryController::class,'viewSport']);
 Route::get('kids',[CategoryController::class,'viewKids']);
+Route::get('/stores', [AdminController::class, "stores"]);
+Route::get('/admin', [AdminController::class, "adminAdd"]);
+Route::get('/customerList', [AdminController::class, "showCustomer"]);;
+Route::get('/productList', [AdminController::class, "showProduct"]);
+Route::get('/delateProduct/{id}', [AdminController::class, "delateProduct"]);
+Route::post('/saveProduct', [ProductController::class, "add_product"]);
+Route::get('/updateView', [ProductController::class, "updateView"]);
+});
 
-//  Route::post('/create', [CreateNewUser::class, 'create']);
